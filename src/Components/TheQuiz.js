8576a69d611fb16866da33d1cf0c4a08b5a21970
Jessica <contact.jessicasanchez@gmail.com>
2020-07-30 16:53:57 -0400

    finished the strength questions

--- src/Components/TheQuiz.js
+++ src/Components/TheQuiz.js
@@ -155,9 +155,110 @@ const QuestionAndAnswers = [
         question: "My body...",
         answers: [
           {
+            answer: "is in pretty good shape, given my age.",
+            value: 1,
+          },
+          {
+            answer: "is on par with most professional athletes.",
+            value: 2,
+          },
+          {
+            answer: "is in superb shape.",
+            value: 3,
+          },
+          {
+            answer: "is out of shape, when compared to almost everyone else.",
+            value: 0,
+          },
+        ]
+      },
+      {
+        question: "I can lift...",
+        answers: [
+          {
+            answer: "about as much as a person of my gender and age should be able to lift.",
+            value: 0,
+          },
+          {
+            answer: "a lot less than I should be able to lift. I am weak",
+            value: -1,
+          },
+          {
+            answer: "noticeably more than most people I know. I am strong.",
+            value: 1,
+          },
+          {
+            answer: "an astounding amount of weight. I engage in extensive weight training.",
+            value: 2,
+          },
+        ]
+      },
+      {
+        question: "The following statement describes the amount of physical labor I regularly perform:",
+        answers: [
+          {
+            answer: "I engage in difficult physical labor all during the week.",
+            value: 2,
+          },
+          {
+            answer: "My job involves physical labor or I have an intensive work-out schedule.",
+            value: 3,
+          },
+          {
+            answer: "My job does not involve much physical labor, but I get enough exercise.",
+            value: 1,
+          },
+          {
+            answer: "Very little, I have a sedentary job and I do not work out.",
+            value: 0,
+          },
+        ]
+      },
+      {
+        question: "The following statement most accurately describes my athletic ability.",
+        answers: [
+          {
+            answer: "I was picked to play for a professional sports team.",
+            value: 2,
+          },
+          {
+            answer: "When being picked for a team, I am always picked first when others know of my ability.",
+            value: 1,
+          },
+          {
+            answer: "I have some athletic ability, but it isn't noteworthy.",
+            value: 0,
+          },
+          {
+            answer: "When being picked for a team, I am always picked last when others know of my ability.",
+            value: -1,
+          },
+        ]
+      },
+    ]
+  },
+  {
+    ability: "dexterity",
+    QuestionSet: [
+      {
+        question: "",
+        answers: [
+          {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -166,7 +267,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -175,7 +288,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -184,7 +309,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
     ]
@@ -198,7 +335,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -207,7 +356,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -216,7 +377,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -225,89 +398,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
-        ]
-      },
-    ]
-  },
-  {
-    ability: "",
-    QuestionSet: [
-      {
-        question: "",
-        answers: [
+          },
           {
             answer: "",
             value: num,
-          }
-        ]
-      },
-      {
-        question: "",
-        answers: [
+          },
           {
             answer: "",
             value: num,
-          }
-        ]
-      },
-      {
-        question: "",
-        answers: [
+          },
           {
             answer: "",
             value: num,
-          }
-        ]
-      },
-      {
-        question: "",
-        answers: [
-          {
-            answer: "",
-            value: num,
-          }
-        ]
-      },
-    ]
-  },
-  {
-    ability: "",
-    QuestionSet: [
-      {
-        question: "",
-        answers: [
-          {
-            answer: "",
-            value: num,
-          }
-        ]
-      },
-      {
-        question: "",
-        answers: [
-          {
-            answer: "",
-            value: num,
-          }
-        ]
-      },
-      {
-        question: "",
-        answers: [
-          {
-            answer: "",
-            value: num,
-          }
-        ]
-      },
-      {
-        question: "",
-        answers: [
-          {
-            answer: "",
-            value: num,
-          }
+          },
         ]
       },
     ]
@@ -321,7 +424,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -330,7 +445,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -339,7 +466,19 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
       {
@@ -348,7 +487,108 @@ const QuestionAndAnswers = [
           {
             answer: "",
             value: num,
-          }
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+        ]
+      },
+    ]
+  },
+  {
+    ability: "",
+    QuestionSet: [
+      {
+        question: "",
+        answers: [
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+        ]
+      },
+      {
+        question: "",
+        answers: [
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+        ]
+      },
+      {
+        question: "",
+        answers: [
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+        ]
+      },
+      {
+        question: "",
+        answers: [
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
+          {
+            answer: "",
+            value: num,
+          },
         ]
       },
     ]
