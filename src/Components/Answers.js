eb0587b14c79817db982ca36996be0ed29cb4015
Jessica <contact.jessicasanchez@gmail.com>
2020-07-30 14:54:46 -0400

    arranging logic

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,75 +1,8 @@
 import React from 'react';
 
-const answers = {
-  1: [
-    {
-      ability: "strength",
-      answer: "What else is a battle-axe good for?"
-    },
-    {
-      ability: "dexterity",
-      answer: "They’ll cease to be a problem long before they even see you"
-    },
-    {
-      ability: "constitution",
-      answer: "Fist-a-cuffs you’ll batter the debt until it’s settled"
-    },
-    {
-      ability: "intelligence",
-      answer: "A plan as already been set in motion, by the end of the day they’ll pay back the debt."
-    },
-    {
-      ability: "charisma",
-      answer: "You’re a silver tongue, you’ll get what’s owed twice over."
-    },
-  ],
-  2: [
-    {
-      ability: "strength",
-      answer: "A Warhammer",
-    },
-    {
-      ability: "dexterity",
-      answer: "Bow and arrows",
-    },
-    {
-      ability: "constitution",
-      answer: "I am a weapon",
-    },
-    {
-      ability: "intelligence",
-      answer: "A spell book",
-    },
-    {
-      ability: "charisma",
-      answer: "My words are daggers",
-    },
-  ],
-  3: [
-    {
-      ability: "strength",
-      answer: "Athletic",
-    },
-    {
-      ability: "dexterity",
-      answer: "Nimble",
-    },
-    {
-      ability: "constitution",
-      answer: "Tenacious",
-    },
-    {
-      ability: "intelligence",
-      answer: "Curious",
-    },
-    {
-      ability: "charisma",
-      answer: "Smooth",
-    },
-  ],
-}
 
-const Answers = () => {
+
+const Answers = (question) => {
   return (
     <div>
       
--- src/Components/TheQuiz.js
+++ src/Components/TheQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Question from './Question';
+import Answers from './Answers';
 
 const questionBank = [
   "You’re contracted to settle a debt. How do you complete the contract?",
@@ -24,6 +25,75 @@ shuffleArray(index)
 
 console.log("this be index", index)
 
+const answers = {
+  1: [
+    {
+      ability: "strength",
+      answer: "What else is a battle-axe good for?"
+    },
+    {
+      ability: "dexterity",
+      answer: "They’ll cease to be a problem long before they even see you"
+    },
+    {
+      ability: "constitution",
+      answer: "Fist-a-cuffs you’ll batter the debt until it’s settled"
+    },
+    {
+      ability: "intelligence",
+      answer: "A plan as already been set in motion, by the end of the day they’ll pay back the debt."
+    },
+    {
+      ability: "charisma",
+      answer: "You’re a silver tongue, you’ll get what’s owed twice over."
+    },
+  ],
+  2: [
+    {
+      ability: "strength",
+      answer: "A Warhammer",
+    },
+    {
+      ability: "dexterity",
+      answer: "Bow and arrows",
+    },
+    {
+      ability: "constitution",
+      answer: "I am a weapon",
+    },
+    {
+      ability: "intelligence",
+      answer: "A spell book",
+    },
+    {
+      ability: "charisma",
+      answer: "My words are daggers",
+    },
+  ],
+  3: [
+    {
+      ability: "strength",
+      answer: "Athletic",
+    },
+    {
+      ability: "dexterity",
+      answer: "Nimble",
+    },
+    {
+      ability: "constitution",
+      answer: "Tenacious",
+    },
+    {
+      ability: "intelligence",
+      answer: "Curious",
+    },
+    {
+      ability: "charisma",
+      answer: "Smooth",
+    },
+  ],
+}
+
 const TheQuiz = () => {
   return (
     <div>
@@ -31,8 +101,9 @@ const TheQuiz = () => {
         index.map((index, key) => {
           return(
           <Question key={key} name= "quiz__Question" question={questionBank[index]}/>
-        )})
-      }
+          )})
+        }
+        <Answers /> 
     </div>
   );
 };
