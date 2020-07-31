4df6220ad72f2fd61c6e377d35296efa857802a6
Jessica <contact.jessicasanchez@gmail.com>
2020-07-31 14:38:20 -0400

    going tru the sub questions

--- src/Components/TheQuiz.js
+++ src/Components/TheQuiz.js
@@ -5,15 +5,25 @@ import {index, questionSetIndex, ansIndex1, ansIndex2} from '../IndexExtractor'
 import { Button } from '@material-ui/core';
 
 
+//
+// ─── VARIABLES ──────────────────────────────────────────────────────────────────
+//
 
 let num = 0;
 let num1 = 0;
-let next = index.splice(num, 1);  
-
-
+let next = index.splice(num, 1); 
+let altNext = questionSetIndex.splice(num, 1); 
+//
 // console.log("this be index", index)
 // console.log("this be questionSetIndex", questionSetIndex)
 // console.log("this be Response", QuestionsAndAnswer[0].Response[0])
+
+// ────────────────────────────────────────────────────────────────────────────────
+
+
+//
+// ─── START OF COMPONENT ─────────────────────────────────────────────────────────
+//
 
 const TheQuiz = () => {
 
@@ -23,25 +33,36 @@ const TheQuiz = () => {
     setNexQue(QuestionsAndAnswer[next[num1]].Question);
   }, []);
 
-  console.log("next!", next)
-  console.log("num!", num)
+  // console.log("next!", next)
+  // console.log("num!", num)
+  
+
+  /*----------  The following gives the index for the next question on click  ----------*/
+let totalQuestions = 0;
   const nextQuestion = () => {
     // num++; 
-    // console.log("next!", next)
+    console.log("next!", next)
+    console.log("alt next!", altNext)
     // console.log("index length is", index.length)
-    next = index.splice(num-1, 1);
+    console.log("altnext length is", questionSetIndex.length)
+    next = index.splice(num, 1);
     if (index.length !== 0) {
       if(next < 5){
         setNexQue(QuestionsAndAnswer[next[num1]].Question);
-      } else if(next >= 5){
-        setNexQue(QuestionsAndAnswer[next[num1]].QuestionSet[0].question);
+        console.log(totalQuestions++)
+        
+      } else if(next >= 5 && questionSetIndex.length !== 0){
+        altNext = questionSetIndex.splice(num,1);
+        setNexQue(QuestionsAndAnswer[next[num1]].QuestionSet[altNext[num1]].question);
+        console.log(totalQuestions++)
+
       }
     }else{
       alert("no more questions")
     }
   }
   
-  console.log("nexQue is:", nexQue)
+  // console.log("nexQue is:", nexQue)
 
   return (
     <div>
