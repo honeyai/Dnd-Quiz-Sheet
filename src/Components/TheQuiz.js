712e44f406cb83dca0287d3e867cecce69a80ec5
Jessica <contact.jessicasanchez@gmail.com>
2020-07-31 13:20:09 -0400

    trying to get my button to show up

--- src/Components/TheQuiz.js
+++ src/Components/TheQuiz.js
@@ -1,53 +1,12 @@
 import React from 'react';
 import Question from './Question';
 import QuestionsAndAnswer from '../QuestionsAndAnswerBank';
-// import Answers from './Answers';
+import {index, questionSetIndex, ansIndex1, ansIndex2} from '../IndexExtractor'
 import { Button } from '@material-ui/core';
 
-const index = [];
-
-QuestionsAndAnswer.forEach(question => {
-  index.push(QuestionsAndAnswer.indexOf(question))
-})
-
-const shuffleArray = array => {
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-shuffleArray(index)
-
-const ansIndex1 = [];
-const ansIndex2 = [];
-
-index.map(index => {
-  // console.log(index)
-  if(index < 5) {
-    // console.log( "this is the type,", typeof QuestionsAndAnswer[index].Response[index2])
-    QuestionsAndAnswer[index].Response.forEach(object => {
-      if (ansIndex1.length < 5) {
-        ansIndex1.push(QuestionsAndAnswer[index].Response.indexOf(object))
-      }
-    })
-  } else if (index > 5){
-    QuestionsAndAnswer[index].QuestionSet.forEach(object => {
-      if (ansIndex2.length < 4) {
-        ansIndex2.push(QuestionsAndAnswer[index].QuestionSet.indexOf(object))
-      }
-    })
-  }
-});
-
-shuffleArray(ansIndex1);
-shuffleArray(ansIndex2);
-
-console.log("this is ansIndex1,", ansIndex1);
-console.log("this is ansIndex2,", ansIndex2);
-
-let num = 1;
+let num = 0;
 let next = num;  
+
 const nextQuestion = () => {
   num++;
   next = index.splice(num, 1);
@@ -55,6 +14,7 @@ const nextQuestion = () => {
 
 
 // console.log("this be index", index)
+// console.log("this be questionSetIndex", questionSetIndex)
 // console.log("this be Response", QuestionsAndAnswer[0].Response[0])
 
 const TheQuiz = () => {
@@ -62,14 +22,18 @@ const TheQuiz = () => {
     <div>
      { console.log("this is next,", next)}
       {
-        next < 6 ? 
-        <Question name= "quiz__Question" question={QuestionsAndAnswer[next].Question}/>
-        : console.log("what the hell is going on?")
+        next < 5 ? 
+        <div>
+          <Question name= "quiz__Question" question={QuestionsAndAnswer[next].Question}/>
+          <Button onClick={nextQuestion}/>
+        </div>
+        : 
+        <div>
+          <Question name= "quiz__Question" question={QuestionsAndAnswer[next].QuestionSet[0].question}/>
+          <Button onClick={nextQuestion}/>
+        </div>
+
       }
-      {
-        console.log("this is QuestionSet,", QuestionsAndAnswer[next].QuestionSet[0])
-      }
-      <Button onClick={nextQuestion}/>
     </div>
   );
 };
---
+++ src/IndexExtractor.js
@@ -0,0 +1,68 @@
+import QuestionsAndAnswer from './QuestionsAndAnswerBank';
+
+const index = [];
+
+QuestionsAndAnswer.forEach(question => {
+  index.push(QuestionsAndAnswer.indexOf(question))
+})
+
+const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+const questionSetIndex = [];
+
+index.map(index => {
+  if (index > 5) {
+    QuestionsAndAnswer[index].QuestionSet.forEach(question => {
+      if (questionSetIndex.length < 4){
+        questionSetIndex.push(QuestionsAndAnswer[index].QuestionSet.indexOf(question))
+      }
+    })
+  }
+})
+
+
+shuffleArray(index)
+shuffleArray(questionSetIndex)
+
+
+const ansIndex1 = [];
+const ansIndex2 = [];
+
+index.map(index => {
+  // console.log(index)
+  if(index < 5) {
+    // console.log( "this is the type,", typeof QuestionsAndAnswer[index].Response[index2])
+    QuestionsAndAnswer[index].Response.forEach(object => {
+      if (ansIndex1.length < 5) {
+        ansIndex1.push(QuestionsAndAnswer[index].Response.indexOf(object))
+      }
+    })
+  } else if (index > 5){
+    QuestionsAndAnswer[index].QuestionSet.forEach(object => {
+      if (ansIndex2.length < 4) {
+        ansIndex2.push(QuestionsAndAnswer[index].QuestionSet.indexOf(object))
+      }
+    })
+  }
+});
+
+shuffleArray(ansIndex1);
+shuffleArray(ansIndex2);
+
+console.log("this is ansIndex1,", ansIndex1);
+console.log("this is ansIndex2,", ansIndex2);
+
+console.log("this is questionSetIndex,", questionSetIndex)
+
+
+
+export {
+  index, 
+  questionSetIndex,
+  ansIndex1,
+  ansIndex2};
