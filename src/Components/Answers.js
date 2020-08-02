1320b73bd267db5a76a0fcd4ef1486b1ddb2bdfb
Jessica <contact.jessicasanchez@gmail.com>
2020-08-02 13:12:29 -0400

    state is not my friend, changing from state

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,21 +1,63 @@
 import React, { useState } from "react";
-import { Button}  from "@material-ui/core";
+// import { Button}  from "@material-ui/core";
 
-const Answers = ({answer, value, attribute }) => {
+let finalScoreSet ;
 
-  const [choice, setChoice] = useState({
-    strength: null,
-    dexterity: null,
-    constitution: null,
-    intelligence: null,
-    charisma: null
-  })
+const Answers = ({ answer, value, attribute }) => {
+  let [strength, setStr] = useState(0);
+  let [dexterity, setDex] = useState(0);
+  let [constitution, setCon] = useState(0);
+  let [intelligence, setInt] = useState(0);
+  let [charisma, setCha] = useState(0);
 
-  const handleClick = event => {
+  // console.log("this is the value added hopefully,", choice.strength += +event.target.value)
+  
+  const handleClick = (event) => {
+    // finalScoreSet = {
+    //   strength: strength,
+    //   dexterity: dexterity,
+    //   constitution: constitution,
+    //   intelligence: intelligence,
+    //   charisma: charisma,
+    // }
+    console.log("this is hopefully attribute,", event.target.dataset.attribute);
     event.preventDefault();
-    console.log("this is hopefully attribute,", event.target.dataset.attribute)
-  }
-
+    switch (event.target.dataset.attribute) {
+      case "strength":
+        setStr(strength += +event.target.value);
+        break;
+      case "dexterity":
+        setDex(dexterity += +event.target.value);
+        break;
+      case "constitution":
+        setCon(constitution += +event.target.value);
+        break;
+      case "intelligence":
+        setInt(intelligence += +event.target.value);
+        break;
+      case "charisma":
+        setCha(charisma += +event.target.value);
+        break;
+      case "strength,dexterity":
+        setStr(strength += +event.target.value)
+        setDex(dexterity += +event.target.value)
+      default:
+        console.log("no choices have been made");
+    }
+    // finalScoreSet = {
+    //   strength: strength,
+    //   dexterity: dexterity,
+    //   constitution: constitution,
+    //   intelligence: intelligence,
+    //   charisma: charisma,
+    // }
+  };
+  console.log("hopefully, strength will update and not reset,", strength);
+  console.log("hopefully, dexterity will update and not reset,", dexterity);
+  console.log("hopefully, constitution will update and not reset,", constitution);
+  console.log("hopefully,intelligence will update and not reset,",intelligence);
+  console.log("hopefully, charisma will update and not reset,", charisma);
+  
   return (
     <div>
       <button
@@ -24,12 +66,9 @@ const Answers = ({answer, value, attribute }) => {
         data-attribute={attribute}
       >
         {answer}
-
       </button>
-      {/* <label htmlFor="answer"> */}
-      {/* </label> */}
     </div>
   );
 };
 
-export default Answers;
+export default ({ finalScoreSet }, Answers);
